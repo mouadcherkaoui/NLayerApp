fb2dae5017f091204118cb2f4894296001f75c2f
cherkaoui mouad <cherkaoui.mouad@live.fr>
2019-09-09 17:52:27 +0100

    adding NLayerApp.CognitiveAIApis.(Infrastructure/Services) projects to the solution

---
+++ NLayerApp.Blazor.WebAssembly/Models/Wrapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace NLayerApp.Blazor.WebAssembly.Models
+{
+    public class IndexedWrapper<T>: ObservableObject
+    {
+        Dictionary<string, object> _properties;
+        T _value;
+        public IndexedWrapper(T value)
+        {
+            _value = value;
+            _properties = typeof(T).GetProperties().ToDictionary(t => t.Name,
+                t => t.GetValue(_value));
+        }
+        public object this[string index]
+        {
+            get => _properties[index];
+            set { var property = _properties[index]; RaisePropertyChanged(ref property, value); }
+        }
+    }
+}
